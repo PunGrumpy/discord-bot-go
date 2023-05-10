1b147ca643a6b49d00a615979cebe55c6fbf8272
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-10 21:09:08 +0700

    :sparkles: add ratelimit on server

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/bwmarrin/discordgo v0.27.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/time v0.3.0
 )
 
 require (
--- pkg/bot/server.go
+++ pkg/bot/server.go
@@ -3,10 +3,24 @@ package bot
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
+var limiter = rate.NewLimiter(1, 5) // allows 1 request per second with a burst of 5
+
+func rateLimit(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+		next(w, r)
+	}
+}
+
 func (bot *Bot) StartServer() {
-	http.HandleFunc("/health-check", bot.healthCheckHandler)
+	http.HandleFunc("/health-check", rateLimit(bot.healthCheckHandler))
 
 	err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil)
 	if err != nil {
